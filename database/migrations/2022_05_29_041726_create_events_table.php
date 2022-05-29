497e5d42d69bfe7c4b2d2018fadbc0c8b7096f63
Adam Haley <adamhaley@gmail.com>
2022-05-28 21:50:51 -0700

    Created Model, Controller, Migration for Events, added view

--- database/migrations/2022_05_29_041726_create_events_table.php
+++ database/migrations/2022_05_29_041726_create_events_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->dateTime('start');
+            $table->dateTime('end');
+
             $table->timestamps();
         });
     }
---
+++ resources/views/full-calendar.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JJGym Calendar</title>
+
+    <meta name="csrf-token" href="{{csrf_token()}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <br />
+    <h1 class="text-center text-primary"></h1>
+    <br />
+</div>
+
+<script>
+    $(document).ready(function(){
+        $.ajaxSetup({
+            headers:{
+                'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content');
+            }
+        });
+
+        var calendar = $('#calendar').fullCalendar();
+    });
+
+</script>
+
+</body>
+</html>
