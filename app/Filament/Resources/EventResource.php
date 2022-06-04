94104185048cf6f686ee3475678dca8204992c6d
Adam Haley <adamhaley@gmail.com>
2022-06-03 21:23:28 -0700

    Added Event Resource

---
+++ app/Filament/Resources/EventResource.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EventResource\Pages;
+use App\Filament\Resources\EventResource\RelationManagers;
+use App\Models\Event;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class EventResource extends Resource
+{
+    protected static ?string $model = Event::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id'),
+                Tables\Columns\TextColumn::make('title')
+                    ->label('Title')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('start')
+                    ->label('Start Time')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('end')
+                    ->label('End Time')
+                    ->date()
+                    ->sortable(),
+            ])
+            ->defaultSort('start','desc')
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEvents::route('/'),
+            'create' => Pages\CreateEvent::route('/create'),
+            'edit' => Pages\EditEvent::route('/{record}/edit'),
+        ];
+    }
+}
