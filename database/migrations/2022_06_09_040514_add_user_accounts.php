40753fa021502871d4c48bc5999cdbdcf3002385
Adam Haley <adamhaley@Adams-Computer.local>
2022-06-08 21:11:25 -0700

    Added test user accounts

---
+++ database/migrations/2022_06_09_040514_add_user_accounts.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //add admin accounts for Adam, Jack, and Jeri
+        DB::table('users')->insert(
+            array(
+                'name' => 'Adam Haley'
+                'email' => 'adamhaley@gmail.com',
+                'verified' => true,
+                'password' => 'password'
+            )
+        );
+        DB::table('users')->insert(
+            array(
+                'name' => 'Jeri Kalvan'
+                'email' => 'jeri@kalvan.net',
+                'verified' => true,
+                'password' => 'password'
+            )
+        );
+        DB::table('users')->insert(
+            array(
+                'name' => 'Jack Kalvan'
+                'email' => 'jack@kalvan.net',
+                'verified' => true,
+                'password' => 'password'
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        DB::table('users')->where('name', 'Adam Haley')->delete();
+        DB::table('users')->where('name', 'Jack Kalvan')->delete();
+        DB::table('users')->where('name', 'Jeri Kalvan')->delete();
+    }
+};
