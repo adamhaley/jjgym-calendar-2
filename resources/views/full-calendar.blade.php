e57a729f1bb87f950d04e052d15e8ed01a0f2b6e
Adam Haley <adamhaley@Adams-Computer.local>
2022-06-26 15:32:32 -0700

    Added boostrap modal on date select, removed un-needed code

--- resources/views/full-calendar.blade.php
+++ resources/views/full-calendar.blade.php
@@ -7,8 +7,10 @@
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
     <script src="js/app.js"></script>
@@ -21,6 +23,28 @@
     <br />[<a href="admin/">Admin</a>] [<a href="#" class="request-time">Request Time</a>]
     <span class="cta float-right d-none text-primary">Click anywhere on the calendar to request a time slot:</span>
     <div id="calendar"></div>
+</div>
+
+<div class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Request Time</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Submit</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+            </div>
+        </div>
+    </div>
 </div>
 
 <script>
@@ -40,16 +64,15 @@
             events:'/calendar',
             selectable:true,
             selectHelper:true,
-            /*
-            select:function(start,end,allDay)
+            select: (start,end,allDay) =>
             {
+                $('.modal').modal('show');
+                /*
                 var date = $.fullCalendar.formatDate(start, 'Y-MM-DD');
                 var friendlyStart = $.fullCalendar.formatDate(start, 'HH:mm:ss');
                 var friendlyEnd = $.fullCalendar.formatDate(end, 'HH:mm:ss');
-                var title = prompt('Inquire about gym use on ' + date + ' from ' + friendlyStart + ' to ' + friendlyEnd + '?\n' + 'Event Title:');
 
-                if(title)
-                {
+                $('form').submit(() => {
                     var start = $.fullCalendar.formatDate(start, 'Y-MM-DD HH:mm:ss');
                     var end = $.fullCalendar.formatDate(end, 'Y-MM-DD HH:mm:ss');
 
@@ -68,9 +91,9 @@
                             alert("Your request has been sent. An Admin will get back to you shortly!");
                         }
                     })
-                }
+                });
+                */
             },
-             */
             editable:false,
             eventColor: '#fff',
             minTime:'08:00',
@@ -125,28 +148,6 @@
                 });
             },
             defaultView: window.innerWidth <= 765 ?  "listMonth": "month",
-            /*
-            eventClick:function(event)
-            {
-                if(confirm("Are you sure you want to remove it?"))
-                {
-                    var id = event.id;
-                    $.ajax({
-                        url:"/calendar/action",
-                        type:"POST",
-                        data:{
-                            id:id,
-                            type:"delete"
-                        },
-                        success:function(response)
-                        {
-                            calendar.fullCalendar('refetchEvents');
-                            alert('Event deleted successfully');
-                        }
-                    })
-                }
-            }
-             */
         });
         console.log('created calendar');
         $('.request-time').click((e) => {
